2050ae7dcae604b7f3fb430a8b0f7a2b4374b668
Cyril Medabalimi <cyrildmed@gmail.com>
2025-01-25 22:08:08 -0500

    before adding asset pack for levers

---
+++ Assets/Scripts/CrankMotion.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class CrankMotion:MonoBehaviour
+{
+    //public UnityEvent<bool> OnLeverStateChanged;
+    //public Transform grabObject;
+
+    //private Vector3 _relativePos;
+    //private bool _isGrabbed;
+
+    //private void Awake()
+    //{
+    //    if (OnLeverStateChanged == null) 
+    //    {
+    //        OnLeverStateChanged = new UnityEvent<bool>();
+    //    }
+
+
+    //}
+
+    //private void Update()
+    //{
+    //    _relativePos = this.transform.InverseTransformPoint(grabObject.position);
+    //}
+
+    //public void ReturnGrabCollider()
+    //{
+    //    grabObject.position = this.transform.TransformPoint(new Vector3(_relativePos.x, 0, _relativePos.z));
+    //}
+
+
+    //public void StartTrac
+
+    //private void OnActivate()
+
+    //[SerializeField] private XRGrabInteractable grabInteractable;
+    //public UnityEvent OnGrabbed;
+    //public UnityEvent OnReleased;
+
+    //public bool isGrabbed;
+
+    //private void Awake()
+    //{
+    //    grabInteractable = GetComponentInChildren<XRGrabInteractable>();
+    //}
+
+}
